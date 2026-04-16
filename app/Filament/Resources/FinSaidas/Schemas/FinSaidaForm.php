a664fbb6178f8fbd8c1e9828f174e9bf5302b77f
wbsartori <wbsartori@gmail.com>
2026-04-15 23:39:09 -0300

    feat(personal-finance): Personal Finance

--- app/Filament/Resources/FinSaidas/Schemas/FinSaidaForm.php
+++ app/Filament/Resources/FinSaidas/Schemas/FinSaidaForm.php
@@ -2,9 +2,15 @@
 
 namespace App\Filament\Resources\FinSaidas\Schemas;
 
+use App\Enums\FormaPagamento;
+use App\Enums\StatusPagamento;
+use App\Enums\TipoLancamento;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\RawJs;
 
 class FinSaidaForm
 {
@@ -12,28 +18,36 @@ class FinSaidaForm
     {
         return $schema
             ->components([
-                TextInput::make('users_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('descricao'),
-                TextInput::make('valor')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
-                TextInput::make('forma_pagamento')
-                    ->required()
-                    ->default('DEB'),
-                TextInput::make('tipo_lancamento')
-                    ->default('A'),
-                TextInput::make('numero_parcela')
-                    ->default('0'),
-                TextInput::make('cartao_credito'),
-                DatePicker::make('data_vencimento')
-                    ->required(),
-                DatePicker::make('data_pagamento'),
-                TextInput::make('status')
-                    ->required()
-                    ->default('P'),
+                Section::make()->schema([
+                    TextInput::make('users.name')->label('Responsável pela entrada'),
+                    TextInput::make('descricao'),
+                    TextInput::make('valor')
+                        ->required()
+                        ->mask(RawJs::make(<<<'JS'
+                            $money($input, ',', '.', 2)
+                            JS)
+                        )
+                        ->default(0),
+                    Radio::make('forma_pagamento')
+                        ->inline()
+                        ->options(FormaPagamento::toOptions())
+                        ->default('DEB'),
+                    Radio::make('tipo_lancamento')
+                        ->inline()
+                        ->options(TipoLancamento::toOptions())
+                        ->default('A'),
+                    TextInput::make('numero_parcela')
+                        ->label('Número da Parcela')
+                        ->default('0'),
+                    DatePicker::make('data_vencimento')
+                        ->required(),
+                    DatePicker::make('data_pagamento'),
+                    Radio::make('status')
+                        ->inline()
+                        ->options(StatusPagamento::toOptions())
+                        ->required()
+                        ->default('P'),
+                ])->columnSpanFull()
             ]);
     }
 }
